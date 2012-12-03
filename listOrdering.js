2775ffbbccfc8c9e6ecfaf40681315ea276be827
Ethan H. Jurman <ethanjurman@gmail.com>
2012-12-03 07:31:42 -0500

    made function for creation of buttons

--- listOrdering.js
+++ listOrdering.js
@@ -41,18 +41,18 @@ window.onload = function() { // on window load, run the application
         },
         
         appendModelView: function(model) {
-            console.log(thiss);
+            console.log(this);
             this.$el.append(new BackboneList.View.Item({model: model}).render().el);
         },
         
-        moveUp: function(event, model, index, name) { 
-            console.log(this.input);
+        moveUp: function(event, model, row) { 
+            console.log(model);
             
             this.render();
         },
         
-        moveDn: function(event, model, index, name) { 
-            console.log(event);
+        moveDn: function(event, model, row) { 
+            console.log(model);
             
             this.render();
         }
@@ -62,14 +62,14 @@ window.onload = function() { // on window load, run the application
     var Instance = {};
     // Instance collection, new Object from BackboneList (from Backbone.Collection)
     Instance.collection = new BackboneList.Collection.Items();
-    bA = "<input type='submit' value='v' id='bA' />";
-    bB = "<input type='submit' value='v' id='bB' />";
-    Instance.collection.add(new BackboneList.Model.Item({index: 0,name: 'Steve Jobs',sIndex: 0,button1:bA,button2:bB}));
-    Instance.collection.add(new BackboneList.Model.Item({index: 1,name: 'Bill Gates',sIndex: 1,button1:bA,button2:bB}));
-    Instance.collection.add(new BackboneList.Model.Item({index: 2,name: 'Mark Zuckerberg',sIndex: 2,button1:bA,button2:bB}));
-    Instance.collection.add(new BackboneList.Model.Item({index: 3,name: 'Elon Musk',sIndex: 3,button1:bA,button2:bB}));
-    Instance.collection.add(new BackboneList.Model.Item({index: 4,name: 'Larry Paige',sIndex: 4,button1:bA,button2:bB}));
-    Instance.collection.add(new BackboneList.Model.Item({index: 5,name: 'Larry Ellison',sIndex: 5,button1:bA,button2:bB}));
+    buttonAB = function(button) { if (button=="A") {return 1;} else { return 0;} };
+    buttonF = function(button,index) { return "<input type='submit' class='clickable' value="+['^','v'][buttonAB(button)]+" row="+index+" id='b"+button+"' />"; }
+    Instance.collection.add(new BackboneList.Model.Item({index: 0,name: 'Steve Jobs',sIndex: 0,button1:buttonF("A",0),button2:buttonF("B",0)}));
+    Instance.collection.add(new BackboneList.Model.Item({index: 1,name: 'Bill Gates',sIndex: 1,button1:buttonF("A",1),button2:buttonF("B",1)}));
+    Instance.collection.add(new BackboneList.Model.Item({index: 2,name: 'Mark Zuckerberg',sIndex: 2,button1:buttonF("A",2),button2:buttonF("B",2)}));
+    Instance.collection.add(new BackboneList.Model.Item({index: 3,name: 'Elon Musk',sIndex: 3,button1:buttonF("A",3),button2:buttonF("B",3)}));
+    Instance.collection.add(new BackboneList.Model.Item({index: 4,name: 'Larry Paige',sIndex: 4,button1:buttonF("A",4),button2:buttonF("B",4)}));
+    Instance.collection.add(new BackboneList.Model.Item({index: 5,name: 'Larry Ellison',sIndex: 5,button1:buttonF("A",5),button2:buttonF("B",5)}));
 
     // The collection view comes from the BackboneList (from Backbone.View)
     Instance.collectionView = new BackboneList.View.Items({
